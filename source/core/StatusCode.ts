6e340418b03f5e2c289bf6b2079660068f881edb
Letícia <leticia_machadolopes@hotmail.com>
2023-07-29 19:47:17 -0300

    feat: adds status code enum

---
+++ source/core/StatusCode.ts
@@ -0,0 +1,13 @@
+export enum StatusCode{
+    OK = 200,
+    CREATED = 201,
+    ACCEPTED = 202,
+    NO_CONTENT = 204,
+    MOVED_PER = 301,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    UNPROCESSABLE = 422,
+    SERVER_ERROR = 500
+}
