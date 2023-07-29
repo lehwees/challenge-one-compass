75af4b0a62ff3aea8f3e8e9b5ac55324d79803d4
Letícia <leticia_machadolopes@hotmail.com>
2023-07-29 19:23:11 -0300

    refactor: improves http service express implementation

---
+++ source/presenters/infra/HttpServiceExpress.ts
@@ -0,0 +1,46 @@
+import { HttpServerService } from '@/services/httpServer/HttpServerService';
+import express, { Application } from 'express';
+import { controllerList } from '../controllers';
+import { Reflection } from '@abraham/reflection';
+import { RouteItem } from '@/decorators/handlers';
+
+export class HttpServiceExpress implements HttpServerService {
+	private readonly instance: Application;
+
+	constructor() {
+		this.instance = express();
+	}
+
+	init(port: string): void {
+		this.registerRoutes();
+		this.instance.listen(port, () => console.log('Server Initialize'));
+	}
+	registerRoutes(): void {
+		const info: { api: string; handler: string }[] = [];
+
+		controllerList.forEach((controllerClass) => {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const _instance = new controllerClass() as any;
+			const basePath: string =
+        Reflection.getMetadata('basePath', controllerClass) ?? '';
+			const routers: RouteItem[] =
+        Reflection.getMetadata('routers', controllerClass) ?? [];
+
+			const expressRouter = express.Router();
+
+			routers.forEach(({ method, path, handlerName }) => {
+				expressRouter[method](
+					path,
+					_instance[String(handlerName)].bind(_instance)
+				);
+
+				info.push({
+					api: `${method.toLocaleUpperCase()} ${basePath + path}`,
+					handler: `${controllerClass.name}.${String(handlerName)}`,
+				});
+
+				this.instance.use(basePath, expressRouter);
+			});
+		});
+	}
+}
