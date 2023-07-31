7e8572d17331b20f81c0fde29d7686f35e2bece1
Letícia <leticia_machadolopes@hotmail.com>
2023-07-31 00:03:32 -0300

    refactor: improves tutor schema

--- source/schemas/TutorSchema.ts
+++ source/schemas/TutorSchema.ts
@@ -1,22 +1,23 @@
-import { PetSchemaInterface } from './PetSchema';
 import mongoose from 'mongoose';
 
 export interface TutorSchemaInterface{
+    id: number,
     name: string,
     phone: string,
     email: string,
     date_of_birth: string,
     zip_code: string,
-    pets: PetSchemaInterface[]
+    pets: {petId: number}[]
 }
 
 export const TutorSchema = new mongoose.Schema<TutorSchemaInterface>({
+	id: Number,
 	name: String,
 	phone: String,
 	email: String,
 	date_of_birth: String,
 	zip_code: String,
-	pets: String
+	pets: [{petId: Number}]
 });
 
 
