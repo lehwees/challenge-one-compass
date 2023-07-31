4dd94e8d8e04b0e6c67f61e8d52fa23e43240cd6
Letícia <leticia_machadolopes@hotmail.com>
2023-07-31 00:07:17 -0300

    refactor: improves app config

--- source/presenters/app.ts
+++ source/presenters/app.ts
@@ -1,8 +1,11 @@
-import { HttpServiceExpress } from './services/HttpServiceExpress';
+import 'dotenv/config';
+import { HttpServiceExpress } from './infra/HttpServiceExpress';
 
 const PORT: string = process.env.PORT || '3000';
 
 const app = new HttpServiceExpress();
 
 app.init(PORT);
+
+
 
