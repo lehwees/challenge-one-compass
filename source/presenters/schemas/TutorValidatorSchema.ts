1e4426c8bf4cf5a9a0a6119c9e234d7364bd2451
Letícia <leticia_machadolopes@hotmail.com>
2023-07-31 00:04:45 -0300

    refactor: improves tutor validator schema

--- source/presenters/schemas/TutorValidatorSchema.ts
+++ source/presenters/schemas/TutorValidatorSchema.ts
@@ -1,6 +1,7 @@
-import { object, string } from 'yup';
+import { number, object, string } from 'yup';
 
 export const tutorValidatorSchema = object({
+	id: number().required(),
 	name: string().required(),
 	phone: string().required(),
 	email: string().required(),
